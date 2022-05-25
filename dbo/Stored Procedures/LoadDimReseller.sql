b12786dae528f6c26a7af3d7d6165b7fb539ec5f
alicja <alicchm892@student.polsl.pl>
2022-05-25 15:15:18 +0200

    add dimReseller

---
+++ dbo/Stored Procedures/LoadDimReseller.sql
@@ -0,0 +1,33 @@
+﻿CREATE procedure [dbo].[LoadDimReseller] as
+	
+	drop table if exists #resellers
+
+	select 
+		C.CustomerID as [CustomerID],
+		C.AccountNumber as [ResellerAlternateKey],
+		S.Name as [ResellerName]
+	into #resellers
+	from [stg].[Sales_Customer] C
+	join [stg].[Sales_Store] S on C.StoreID = S.BusinessEntityID
+
+	--select * from #resellers
+
+	update a
+	set
+		[CustomerID] = b.[CustomerID],
+		[ResellerAlternateKey] = b.[ResellerAlternateKey],
+		[ResellerName] = b.[ResellerName],
+		[ModifiedDate] = getdate()
+	from [dbo].[dimReseller] a
+	join #resellers b on a.CustomerID = b.CustomerID
+	where
+		a.[CustomerID] != b.[CustomerID]
+		or a.[ResellerAlternateKey] != b.[ResellerAlternateKey]
+		or a.[ResellerName] != b.[ResellerName]
+
+	insert into [dbo].[dimReseller](
+		[CustomerID], [ResellerAlternateKey], [ResellerName], [CreatedDate], [ModifiedDate])
+	select b.*, getdate(), getdate()
+	from [dbo].dimReseller a
+	right join #resellers b on a.CustomerID = b.CustomerID
+	where a.CustomerID is null
---
+++ dbo/Tables/dimReseller.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[dimReseller] (
+    [ResellerKey]          INT          IDENTITY (1, 1) NOT NULL,
+    [CustomerID]           INT          NOT NULL,
+    [ResellerAlternateKey] VARCHAR (10) NULL,
+    [ResellerName]         VARCHAR (50) NOT NULL,
+    [CreatedDate]          DATETIME     DEFAULT (getdate()) NULL,
+    [ModifiedDate]         DATETIME     NULL
+);
+
